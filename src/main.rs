34bb78133f5214d49bef198b67e3123bdbda6894
Arnaud PERALTA <arnaud.peralta@gmail.com>
2022-07-03 00:15:49 +0200

    new speed parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::{thread, time::Duration};
+
 use clap::Parser;
 use reqwest::Client;
 use futures::{stream, StreamExt};
@@ -10,39 +12,49 @@ struct Args {
     #[clap(short, long, value_parser)]
     url: String,
 
-    /// Number of times to query the target
-    #[clap(short, long, value_parser, default_value_t = 100)]
+    /// Number of total requests to perform on the API
+    #[clap(short, long, value_parser, default_value_t = 1000)]
     count: usize,
+
+    /// Number of query per second
+    #[clap(short, long, value_parser, default_value_t = 100)]
+    speed: usize,
 }
 
-async fn query_target(url: &String, count: usize) {
+// Delay between queries, we take delay between two query in a same second (1/x) then we converts
+// to ms
+fn delay_between_queries(query_per_second: f64) -> Duration {
+    return Duration::from_millis((1f64 / query_per_second * 1000f64) as u64)
+}
+
+async fn query_target(args: &Args) {
     let client = Client::new();
+    let urls = vec![args.url.clone(); args.count];
+    let delay = delay_between_queries(args.speed as f64);
 
-    let urls = vec![url; count];
-
-    let bodies = stream::iter(urls)
+    // Prepare queries for parallelize calls
+    let requests = stream::iter(urls)
         .map(|url| {
             let client = &client;
             async move {
-                let resp = client.get(url).send().await?;
-                resp.bytes().await
+                client.get(url).send().await
             }
         })
-        .buffer_unordered(count);
+    .buffer_unordered(args.count);
 
-    bodies
-        .for_each(|b| async {
-            match b {
-                Ok(b) => println!("Got {} bytes", b.len()),
-                Err(e) => eprintln!("Got an error: {}", e),
-            }
-        })
-        .await;
+    // Sending queries
+    requests.for_each(|responses| async {
+        match responses {
+            Err(e) => eprintln!("Got an error: {}", e),
+            _ => ()
+        }
+        thread::sleep(delay);
+    }).await;
 }
 
 #[tokio::main]
 async fn main() {
     let args = Args::parse();
 
-    query_target(&args.url, args.count).await;
+    query_target(&args).await;
 }
